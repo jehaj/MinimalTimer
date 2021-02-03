4adceb3d95df4196f3080322c4b1a8b4c42b6546
Nikolaj J-K <kagenogmig@gmail.com>
2021-02-03 21:57:33 +0100

    Setup

---
+++ main.js
@@ -0,0 +1,26 @@
+let spanMinutes = document.getElementById('minutes');
+let spanSeconds = document.getElementById('seconds');
+
+let stringDateQuery = window.location.search.substring(1);
+const querySplit = stringDateQuery.split(':')[0];
+let hourQuery = stringDateQuery.split(':')[0];
+let minuteQuery = stringDateQuery.split(':')[1];
+
+let countdownDate = new Date();
+countdownDate.setHours(parseInt(hourQuery), parseInt(minuteQuery), 0);
+
+let myFunc = setInterval(function() {
+    let now = new Date().getTime();
+    let timeleft = countdownDate - now;
+
+    let minutes = Math.floor((timeleft % (1000*60*60)) / (1000*60));
+    let seconds = Math.floor((timeleft % (1000*60)) / (1000));
+
+    
+    spanMinutes.innerHTML = minutes.toString().padStart(2, '0');
+    spanSeconds.innerHTML = seconds.toString().padStart(2, '0');
+
+    if (minutes == 0 && seconds == 0) {
+        clearInterval(myFunc);
+    }
+}, 1000);
